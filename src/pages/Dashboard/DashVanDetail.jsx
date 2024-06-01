4d555e06c9be1d31a859770da4a2812a3c3e20ce
Kapil Upreti <kapilupretiff@gmail.com>
2024-06-01 22:24:20 +0545

    login

--- src/pages/Dashboard/DashVanDetail.jsx
+++ src/pages/Dashboard/DashVanDetail.jsx
@@ -4,7 +4,7 @@ import Login from "../auth/Login";
 const DashVanDetail = () => {
   return (
     <div>
-      <p>this is detail page of van</p>
+      <p>this is detail page of van if you think so its ok</p>
       <Login />
     </div>
   );
